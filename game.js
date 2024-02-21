f02e2b7835b9363148e58627c598611c4970512d
23a-bam <23a_bam@liceum.ztu.edu.ua>
2024-02-21 09:46:27 +0200

    Додав скроллінг

--- game.js
+++ game.js
@@ -19,6 +19,8 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var score = 0; // кількість очків
+var scoreText; // текстова змінна для очків
 
 function preload() {
     this.load.image('sky', "assets/sky.png");
@@ -33,10 +35,11 @@ function create() {
     // додає небо, починаючи з точки (0, 0)
     this.add.image(0, 0, 'sky').setOrigin(0, 0).setDisplaySize(1920, 1080); // setDisplaySize розтягує
 
-    platforms = this.physics.add.staticGroup();
+    platforms = this.physics.add.group();
 
     createGround(0, 1034, 50, 1, new Array(4, 5, 6, 10, 11, 12));
     createGround(4, 920, 3, 1, new Array());
+    createGround(9, 800, 3, 1, new Array());
 
     player = this.physics.add.sprite(50, 700, 'hero');
     player.setScale(3);
@@ -51,28 +54,29 @@ function create() {
 function createGround(start, y, count, scale, holes) {
     for (let i = start; i < start + count; i++) {
         if (holes === null || !holes.includes(i)) { // якщо не задана діра
-            platforms.create(i * 48 * scale, y, 'tile').setScale(scale);
+            platforms.create(i * 48 * scale, y, 'tile').setScale(scale).setImmovable(true);
         }
     }
+    platforms.children.iterate(function (child) {
+        child.setImmovable(true);
+        child.body.setAllowGravity(false);
+    });
 }
 
 function update() {
     if (cursors.left.isDown) // якщо натиснута стрілка вліво
     {
         player.setVelocityX(-160); // йти вліво
-        player.anims.play('left', true);
         player.flipX = true; // повернути вліво
     }
     else if (cursors.right.isDown) // якщо натиснута стрілка вправо
     {
         player.setVelocityX(160); // йти вправо
-        player.anims.play('left', true); // грати анімацію руху вправо
         player.flipX = false; // повернути вправо
     }
     else // якщо не натиснута стрілка вліво чи вправо
     {
         player.setVelocityX(0); // зупинитись
-        player.anims.play('turn'); // грати анімацію стояння
         // player.flipX = false;
     }
 
@@ -81,4 +85,14 @@ function update() {
         // стрибнути, якщо натиснута стрілка вгору і гравець торкається землі
         player.setVelocityY(-340);
     }
+    if (player.x > 400) {
+        scroll();
+    }
+}
+
+function scroll() {
+    player.x -= 3;
+    platforms.children.iterate(function (child) {
+        child.x -= 3;
+    });
 }
