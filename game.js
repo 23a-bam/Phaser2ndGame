92457360db68f5b0de57f0677a169d6f31e015a3
23a-bam <23a_bam@liceum.ztu.edu.ua>
2024-02-21 09:15:24 +0200

    Підправив фон, додав платформи

--- game.js
+++ game.js
@@ -30,7 +30,21 @@ function preload() {
 }
 
 function create() {
-    
+    // додає небо, починаючи з точки (0, 0)
+    this.add.image(0, 0, 'sky').setOrigin(0, 0).setDisplaySize(1920, 1080); // setDisplaySize розтягує
+
+    platforms = this.physics.add.staticGroup();
+
+    createGround(0, 984, 50, 2, new Array(4, 5, 6, 10, 11, 12));
+    createGround(4, 800, 3, 2, new Array());
+}
+
+function createGround(start, y, count, scale, holes) {
+    for (let i = start; i < start + count; i++) {
+        if (holes === null || !holes.includes(i)) { // якщо не задана діра
+            platforms.create(i * 48 * scale, y, 'tile').setOrigin(0, 0).setScale(scale);
+        }
+    }
 }
 
 function update() {
