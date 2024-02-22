cd65345a57ce9205df6849052bcfc49bdc4f9dd5
23a-bam <158849396+23a-bam@users.noreply.github.com>
2024-02-22 18:49:12 +0200

    Додав хліб і систему очок, змінив деякі швидкості

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 400 },
+            gravity: { y: 0},
             debug: false
         }
     },
@@ -40,17 +40,25 @@ function create() {
     createGround(0, 1034, 50, 1, new Array(4, 5, 6, 10, 11, 12));
     createGround(4, 920, 3, 1, new Array());
     createGround(9, 800, 3, 1, new Array());
+    createGround(14, 665, 11, 1, new Array(17, 18, 19, 20, 21))
 
     player = this.physics.add.sprite(50, 700, 'hero');
     player.setScale(3);
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, platforms);
+    // гравітація для гравця
+    player.body.setGravityY(350);
 
     // реєструє стрілки вліво, вправо, вгору, вниз
     cursors = this.input.keyboard.createCursorKeys();
 
     bread = this.physics.add.group();
-    createBread(300, 750);
+    createBread(250, 850);
+    createBread(1100, 620);
+
+    scoreText = this.add.text(16, 16, 'Очок: 0', { fontSize: '32px', fill: '#000' }); // додати текст до текстової змінної очків, задати його локацію
+
+    this.physics.add.collider(player, bread, collectBread, null, this);
 }
 
 function createGround(start, y, count, scale, holes) {
@@ -69,15 +77,21 @@ function createBread(x, y) {
     bread.create(x, y, 'bread');
 }
 
+function collectBread(player, bread) {
+    bread.disableBody(true, true);
+    score += 1;
+    scoreText.setText('Очок: ' + score);
+}
+
 function update() {
     if (cursors.left.isDown) // якщо натиснута стрілка вліво
     {
-        player.setVelocityX(-160); // йти вліво
+        player.setVelocityX(-120); // йти вліво
         player.flipX = true; // повернути вліво
     }
     else if (cursors.right.isDown) // якщо натиснута стрілка вправо
     {
-        player.setVelocityX(160); // йти вправо
+        player.setVelocityX(120); // йти вправо
         player.flipX = false; // повернути вправо
     }
     else // якщо не натиснута стрілка вліво чи вправо
@@ -91,8 +105,8 @@ function update() {
         // стрибнути, якщо натиснута стрілка вгору і гравець торкається землі
         player.setVelocityY(-340);
     }
-    if (player.x > 400) {
-        scroll(3);
+    if (player.x > 600) {
+        scroll(2);
     }
 }
 
